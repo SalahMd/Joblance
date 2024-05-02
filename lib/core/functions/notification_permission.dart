758021564f40ccec7181cc6a6dae4f9201434b6b
SalahMd <salahaldeenmdaghmesh@gmail.com>
2024-05-02 13:03:08 +0300

    Reciving notifications in terminated mode

---
+++ lib/core/functions/notification_permission.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+getNotificationPermession() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+}
